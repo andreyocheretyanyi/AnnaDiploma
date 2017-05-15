3e99422f9bd1ad2fa961f19375ae00b610879b73
andreyocheretyanyi <andrey.ocheretyanyi@gmail.com>
2017-05-15 18:40:45 +0300

    added update for database.

---
+++ app/src/main/java/ua/com/anna/borodina/annadiploma/views/dialogs/DialogWithMap.java
@@ -0,0 +1,40 @@
+package ua.com.anna.borodina.annadiploma.views.dialogs;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import butterknife.ButterKnife;
+import ua.com.anna.borodina.annadiploma.R;
+import ua.com.anna.borodina.annadiploma.views.fragments.MapFragment;
+
+/**
+ * Created by User on 15.05.2017.
+ */
+
+public class DialogWithMap extends DialogFragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        MapFragment dialog = new MapFragment();
+        View v = inflater.inflate(R.layout.dialog_with_map, container, false);
+        Button b = (Button) v.findViewById(R.id.button_dismiss);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        getChildFragmentManager().beginTransaction().add(R.id.container_for_map, dialog).commit();
+        return v;
+    }
+
+}
